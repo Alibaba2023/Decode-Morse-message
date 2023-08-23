16468c593d68aed6cb87e3ee1aa43599b2cae39f
Sergio Andres Usma <126805818+sergio-usma@users.noreply.github.com>
2023-08-23 18:48:45 -0500

    Add functions to decode morse

--- decode.rb
+++ decode.rb
@@ -6,3 +6,17 @@ MORSE_HASH = {
   '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y',
   '--..' => 'Z'
 }.freeze
+
+def decode_char(char)
+  MORSE_HARSH[char]
+end
+
+def decode_word(string)
+  string.split.map { |char| decode_char(char) }.join
+end
+
+def decode(message)
+  message.split('   ').map { |word| decode_word(word) }.join(' ')
+end
+
+puts decode('- .... .. ...   .. ...   --- ..- .-.   ..-. .. .-. ... -   .-. ..- -... -.--   .--. .-. --- --. .-. .- -- -.-.--')
